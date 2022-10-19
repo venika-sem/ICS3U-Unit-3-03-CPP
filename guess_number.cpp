94fe8a2b2959db20b8ad76308a758351629e142f
Venika Sem <venika.sem@mths.ca>
2022-10-19 02:10:43 +0000

    changed init

---
+++ guess_number.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Venika Sem All rights reserved
+//
+// Created by: Venika Sem
+// Created on: Oct 2022
+// This program guess random number
+
+#include <iostream>
+#include <random>
+
+int main() {
+    int randomNumber;
+    int guess;
+
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution<int> idist(0, 9);
+    randomNumber = idist(rgen);
+
+    // input
+    std::cout << "Enter the number between 0-9: ";
+    std::cin >> guess;
+    std::cout << std::endl;
+
+    // process & 0utput
+    if (guess == randomNumber) {
+        std::cout << "Congratulations, you guessed correct!" << std::endl;
+    } else {
+        std::cout << "Sorry you guessed wrong, the number was "
+                  << ""
+                  << randomNumber << "" << std::endl;
+    }
+    std::cout << "\nDone.";
+}
